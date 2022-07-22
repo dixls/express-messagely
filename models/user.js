7b1ac36469b4200c8b674fba4cbe07b2d94e8768
pixls <tha.pixls@gmail.com>
2022-07-22 17:28:43 -0400

    started adding user class methods

--- models/user.js
+++ models/user.js
@@ -1,6 +1,9 @@
 /** User class for message.ly */
 
-
+const { BCRYPT_WORK_FACTOR } = require("../config");
+const db = require("../db");
+const ExpressError = require("../expressError");
+const bcrypt = require("bcrypt");
 
 /** User of the site. */
 
@@ -10,15 +13,53 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({ username, password, first_name, last_name, phone }) {
+    try {
+      const hashedPW = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+      const result = await db.query(
+        `INSERT INTO users (username, password, first_name, last_name, phone, join_at)
+        VALUES ($1, $2, $3, $4, $5, LOCALTIMESTAMP)
+        RETURNING username, password, first_name, last_name, phone`,
+        [username, hashedPW, first_name, last_name, phone]);
+
+      return result.rows[0];
+    } catch (e) {
+      return next(e);
+    }
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    try {
+      const result = await db.query(
+        `SELECT password FROM users WHERE username = $1`,
+        [username]);
+      const user = result.rows[0];
+      if (user) {
+        if (await bcrypt.compare(password, user.password) === true) {
+          return true;
+        }
+      }
+      return false;
+    } catch (e) {
+      return next(e);
+    }
+  }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) {
+    try {
+      const result = await db.query(
+        `UPDATE users
+        SET last_login_at = CURRENT_TIMESTAMP,
+        WHERE username = $1`,
+        [username]);
+    } catch (e) {
+      return next(e);
+    }
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name, phone}, ...] */
