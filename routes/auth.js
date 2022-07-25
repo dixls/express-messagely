263c066a328fa565ea51bf6845c0693d03befe32
pixls <tha.pixls@gmail.com>
2022-07-25 14:51:56 -0400

    wrote routes for auth and users

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,29 @@
+const express = require("express");
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config");
+const ExpressError = require("../expressError");
+const jwt = require("jsonwebtoken");
+
+const router = new express.Router();
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
-
+router.post("/login", async (res, req, next) => {
+    try {
+        const { username, password } = req.body;
+        if (await User.authenticate(username, password)) {
+            await User.updateLoginTimestamp(username);
+            const token = jwt.sign({ username }, SECRET_KEY);
+            return res.json({ token });
+        }
+        throw new ExpressError("Invalid username or Password", 400);
+    } catch (e) {
+        return next(e);
+    }
+})
 
 /** POST /register - register user: registers, logs in, and returns token.
  *
@@ -11,3 +31,14 @@
  *
  *  Make sure to update their last-login!
  */
+router.post("/register", async (req, req, next) => {
+    try {
+        const user = await User.register(req.body);
+        if (user) {
+            const token = jwt.sign({ username: user.username }, SECRET_KEY);
+            return req.json({ token });
+        }
+    } catch (e) {
+        return next(e);
+    }
+})
--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,37 @@
+const express = require("express");
+const User = require("../models/user");
+const Message = require("../models/message");
+const ExpressError = require("../expressError");
+
+const router = new express.Router();
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
  *
  **/
-
+router.get("/", async (req, res, next) => {
+    try {
+        const results = await User.all();
+        return res.json({ users: results })
+    } catch (e) {
+        return next(e);
+    }
+})
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
-
+router.get("/:username", async (req, res, next) => {
+    try {
+        const results = await User.get(req.params.username);
+        return res.json({ user: results })
+    } catch (e) {
+        return next(e);
+    }
+})
 
 /** GET /:username/to - get messages to user
  *
@@ -21,7 +42,14 @@
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
-
+router.get("/:username/to", async (req, res, next) => {
+    try {
+        const results = await User.messagesTo(req.params.username);
+        return res.json({messages: results});
+    } catch (e) {
+        return next(e);
+    }
+})
 
 /** GET /:username/from - get messages from user
  *
@@ -32,3 +60,11 @@
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+ router.get("/:username/from", async (req, res, next) => {
+    try {
+        const results = await User.messagesFrom(req.params.username);
+        return res.json({messages: results});
+    } catch (e) {
+        return next(e);
+    }
+})
